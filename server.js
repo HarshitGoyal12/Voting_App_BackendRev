251cc2262a0723699937f2d63f206e49c6f18443
Harshit Goyal <harshgoyal1357@gmail.com>
2024-07-30 08:06:02 +0530

    backend-files

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express();
+const db = require('./db');
+require('dotenv').config();
+
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.json()); // req.body
+
+const userRoutes = require('./Routes/userRoutes');
+const candidateRoutes = require('./Routes/candidateRoutes');
+
+// Use the routers
+app.use('/user', userRoutes);
+app.use('/candidate', candidateRoutes);
+const PORT=process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log('listening on port 3000');
+})
